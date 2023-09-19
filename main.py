83df8607dba9bdbb08fbed1e32f247cbb26e5b0b
Noudjichris <noudjichrist87@gmail.com>
2023-09-19 20:45:14 +0100

    Lier la liste des repertoires au projet pour creer les repertoires

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
 from pathlib import Path
 # MBAIGOLNOUDJI CHRISTIAN MASTER II Data Analyst
 list_repertoires = ["data/cleaned","data/processed","data/raw", "docs", "models", "notebooks", "reports", "src"]
+# MBAIGOLNOUDJI CHRISTIAN MASTER II Data Analyst
+list_repertoires = ["data/cleaned","data/processed","data/raw", "docs", "models", "notebooks", "reports", "src"]
