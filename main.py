d5914ad3486aa842e37994be9ed107aa3288ed86
Noudjichris <noudjichrist87@gmail.com>
2023-09-19 20:48:20 +0100

    Creer les dossiers sur le repertoire du projet

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from pathlib import Path
 # MBAIGOLNOUDJI CHRISTIAN MASTER II Data Analyst
 list_repertoires = ["data/cleaned","data/processed","data/raw", "docs", "models", "notebooks", "reports", "src"]
-# MBAIGOLNOUDJI CHRISTIAN MASTER II Data Analyst
-list_repertoires = ["data/cleaned","data/processed","data/raw", "docs", "models", "notebooks", "reports", "src"]
+list(map(lambda x: Path(x).mkdir(parents=True, exist_ok=True), list_repertoires))
