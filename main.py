6e11f8b9621f8cc58ccb241ba41384ab6990a47f
Noudjichris <noudjichrist87@gmail.com>
2023-09-19 20:49:24 +0100

    definir une liste contenant les fichiers necessaires au projet

--- main.py
+++ main.py
@@ -2,3 +2,7 @@ from pathlib import Path
 # MBAIGOLNOUDJI CHRISTIAN MASTER II Data Analyst
 list_repertoires = ["data/cleaned","data/processed","data/raw", "docs", "models", "notebooks", "reports", "src"]
 list(map(lambda x: Path(x).mkdir(parents=True, exist_ok=True), list_repertoires))
+
+
+#creer une liste de fichiers
+list_fichiers = ["LICENSE", "Makefile", "README.md","notebooks/main.ipynb", ".gitignore", "requirements.txt","src/utils.py","src/process.py","src/train.py"]
