91a1a5b62a88c9912e9b266b5daa667146fe7f5a
Noudjichris <noudjichrist87@gmail.com>
2023-09-19 20:50:52 +0100

    Creer les fichiers du programme

--- main.py
+++ main.py
@@ -6,3 +6,6 @@ list(map(lambda x: Path(x).mkdir(parents=True, exist_ok=True), list_repertoires)
 
 #creer une liste de fichiers
 list_fichiers = ["LICENSE", "Makefile", "README.md","notebooks/main.ipynb", ".gitignore", "requirements.txt","src/utils.py","src/process.py","src/train.py"]
+
+#creer les fichierd
+list(map(lambda x: Path(x).touch(exist_ok=True), list_fichiers))
